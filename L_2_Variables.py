2957842712e3050ba765e01c546b8ade30fdc454
Sergey Polyakov <serega-polyakoff@rambler.ru>
2022-05-21 19:54:37 +0300

    Beard_Pithon_L2_Variables

---
+++ L_2_Variables.py
@@ -0,0 +1,12 @@
+print("Hello! Welcome to Coffee!")
+name = input("What is your name?\n")
+print("Hello " + name + ", thank you for coming in today!\n\n\n")
+
+menu = "Black coffee, Espresso, Latte, Cappucino"
+print(name + ", what would you like?\n" + menu)
+order = input()
+print("Sounds good " + name + ", we'll have that " + order + " ready in a moment.")
+
+
+
+
