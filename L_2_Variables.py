d95ba7f8dc11e4aa92b4712ff266b938a4576ceb
Sergey Polyakov <serega-polyakoff@rambler.ru>
2022-05-23 18:00:28 +0300

    Beard_Pithon_L4_ifelse

--- L_2_Variables.py
+++ L_2_Variables.py
@@ -1,14 +1,23 @@
 print("Hello! Welcome to Coffee!")
 name = input("What is your name?\n")
-print("Hello " + name + ", thank you for coming in today!\n\n\n")
+
+if name == "Ben":
+    print("You are not welcome here, Ben!! Get out!!")
+    exit()
+else:
+    print("Hello " + name + ", thank you for coming in today!\n\n")
 
 menu = "Black coffee, Espresso, Latte, Cappucino"
+
 print(name + ", what would you like?\n" + menu)
+
 order = input()
 price = 8
 quantity = input("How many coffees would you like?\n")
 total = price * int(quantity)
+
 print("Thank you. Your total is: " + str(total))
+
 print("Sounds good " + name + ", we'll have your "+ quantity + " " + order + " ready in a moment.")
 
 
